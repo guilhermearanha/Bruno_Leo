4398d7e7b946da344c9b7d603dc52618f1fb49cc
brunocp-liseb <63056784+brunocp-liseb@users.noreply.github.com>
2020-04-02 18:36:46 -0300

    Add files via upload

--- aps1_design.py
+++ aps1_design.py
@@ -7,42 +7,44 @@ import random
 
 #sorteio dado 1 e 2 "Come Out"
 soma = 0
+
+x = random.randint(1, 6)
+y = random.randint(1, 6)
+
 def d1():
-    x = random.randint(1, 6)
-    y = random.randint(1, 6)
     soma = x + y
     return soma
 soma = d1()
 
+gameon = False
 
 #fichas iniciais
 fichas = 100
-
+aposta = 0
 
 print("Você tem: 100 fichas")
-print("Para desistir do jogo a qualquer momento, escreva (desligar)"
+print("Para desistir do jogo a qualquer momento, escreva (desligar)")
 
 
 pergunta = input("Você quer apostar?")
 
-if pergunta == "desligar":
-    print("O jogo acabou!")
+if pergunta == "sim" or pergunta == "s" or pergunta == "Sim":
+    gameon = True
 
-while pergunta == "sim" or pergunta == "s" or pergunta == "Sim" or fichas != 0 and pergunta != "desligar":
-
+while gameon == True and fichas != 0:
+    
     print("Início da rodada.")
-    print("Você tem: {0} fichas".format(fichas))
 
     jogo = input("Qual tipo de aposta você deseja fazer? Pass Line Bet / Field / Any Craps / Twelve:")
     
-    #Pass Line Bet(disponível apenas na rodada Come Out): Se a soma dos dados for 7 ou 11, você ganha a mesma quantidade de fichas que apostou. Já se a soma dos dados for 2, 3 ou 12(Craps), você perde tudo. Agora, caso a soma dos dados de 4, 5, 6, 8, 9 ou 10, você avança para a proxima fase: fase Point.
-    #Field(disponível em qual fase do jogo): Se a soma der 5, 6, 7 ou 8, o jogador perde tudo. Já se a soma der 3, 4, 9, 10, ou 11, o jogador ganha o mesmo valor que apostou.
-    #Any Craps(disponível em qual fase do jogo): Se a soma der 2, 3 ou 12, o jogador ganha 7 vezes o valor de sua aposta. Caso o contrário, ele perde tudo que apostou.
-    #Twelve(disponível em qual fase do jogo): Se a soma der 12, o jogador ganha 30 o valor de sua aposta. Caso o contrário, ele perde tudo.
+#Pass Line Bet(disponível apenas na rodada Come Out): Se a soma dos dados for 7 ou 11, você ganha a mesma quantidade de fichas que apostou. Já se a soma dos dados for 2, 3 ou 12(Craps), você perde tudo. Agora, caso a soma dos dados de 4, 5, 6, 8, 9 ou 10, você avança para a proxima fase: fase Point.
+#Field(disponível em qual fase do jogo): Se a soma der 5, 6, 7 ou 8, o jogador perde tudo. Já se a soma der 3, 4, 9, 10, ou 11, o jogador ganha o mesmo valor que apostou.
+#Any Craps(disponível em qual fase do jogo): Se a soma der 2, 3 ou 12, o jogador ganha 7 vezes o valor de sua aposta. Caso o contrário, ele perde tudo que apostou.
+#Twelve(disponível em qual fase do jogo): Se a soma der 12, o jogador ganha 30 o valor de sua aposta. Caso o contrário, ele perde tudo.
     
     if jogo == "Pass Line Bet" or jogo == "pass line bet":
 #Pass Line Bet(disponível apenas na rodada Come Out): Se a soma dos dados for 7 ou 11, você ganha a mesma quantidade de fichas que apostou. Já se a soma dos dados for 2, 3 ou 12(Craps), você perde tudo. Agora, caso a soma dos dados de 4, 5, 6, 8, 9 ou 10, você avança para a proxima fase: fase Point.
-        
+
         aposta = int(input("Quanto você quer apostar?"))
         
         d1()
@@ -57,6 +59,7 @@ while pergunta == "sim" or pergunta == "s" or pergunta == "Sim" or fichas != 0 a
             #colar aqui
         elif aposta == "desligar":
             print("O jogo acabou!")
+            gameon = False
 
     fichas == fichas
          
@@ -78,6 +81,7 @@ while pergunta == "sim" or pergunta == "s" or pergunta == "Sim" or fichas != 0 a
             fichas += aposta*3
         elif aposta == "desligar":
             print("O jogo acabou!")
+            gameon = False
 
     fichas == fichas
 
@@ -95,6 +99,7 @@ while pergunta == "sim" or pergunta == "s" or pergunta == "Sim" or fichas != 0 a
             print("O jogo acabou!")
         else:
             fichas -= aposta
+            gameon = False
 
     fichas == fichas
 
@@ -110,6 +115,7 @@ while pergunta == "sim" or pergunta == "s" or pergunta == "Sim" or fichas != 0 a
             fichas += aposta*30
         elif aposta == "desligar":
             print("O jogo acabou!")
+            gameon = False
         else:
             fichas -= aposta
         
