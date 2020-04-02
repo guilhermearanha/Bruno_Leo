177597862c78342fb991c80d942699b1b41a4aa8
brunocp-liseb <63056784+brunocp-liseb@users.noreply.github.com>
2020-04-02 15:02:32 -0300

    Add files via upload

--- aps1_design.py
+++ aps1_design.py
@@ -12,18 +12,25 @@ def d1():
     y = random.randint(1, 6)
     soma = x + y
     return soma
+soma = d1()
 
 #fichas iniciais
 fichas = 100
+print("Você tem: 100 fichas")
+print("Para desistir do jogo a qualquer momento, escreva (desligar)"
 
 pergunta = input("Você quer apostar?")
 
-if pergunta == "sim" or pergunta == "s" or pergunta == "Sim":
+if pergunta == "desligar":
+    print("O jogo acabou!")
 
-    print("Início da rodada: fase Come Out")
+while pergunta == "sim" or pergunta == "s" or pergunta == "Sim" or fichas != 0 and pergunta != "desligar":
 
-    jogo = input("Qual tipo de aposta você deseja fazer? Pass Line Bet/ Field/ Any Craps/ Twelve:")
+    print("Início da rodada.")
+    print("Você tem: {0} fichas".format(fichas))
 
+    jogo = input("Qual tipo de aposta você deseja fazer? Pass Line Bet / Field / Any Craps / Twelve:")
+    
     #Pass Line Bet(disponível apenas na rodada Come Out): Se a soma dos dados for 7 ou 11, você ganha a mesma quantidade de fichas que apostou. Já se a soma dos dados for 2, 3 ou 12(Craps), você perde tudo. Agora, caso a soma dos dados de 4, 5, 6, 8, 9 ou 10, você avança para a proxima fase: fase Point.
     #Field(disponível em qual fase do jogo): Se a soma der 5, 6, 7 ou 8, o jogador perde tudo. Já se a soma der 3, 4, 9, 10, ou 11, o jogador ganha o mesmo valor que apostou.
     #Any Craps(disponível em qual fase do jogo): Se a soma der 2, 3 ou 12, o jogador ganha 7 vezes o valor de sua aposta. Caso o contrário, ele perde tudo que apostou.
@@ -44,8 +51,10 @@ if pergunta == "sim" or pergunta == "s" or pergunta == "Sim":
         elif soma == 4 or soma == 5 or soma == 6 or soma == 8 or soma == 9 or soma == 10:
             print("Início da fase Point")
             #colar aqui
+        elif aposta == "desligar":
+            print("O jogo acabou!")
 
-        print("Você tem {0}".format(fichas))
+    fichas == fichas
          
     if jogo == "Field" or jogo == "field":
 #Field(disponível em qual fase do jogo): Se a soma der 5, 6, 7 ou 8, o jogador perde tudo. Já se a soma der 3, 4, 9, 10, ou 11, o jogador ganha o mesmo valor que apostou.
@@ -63,8 +72,10 @@ if pergunta == "sim" or pergunta == "s" or pergunta == "Sim":
             fichas += aposta*2
         elif soma == 12:
             fichas += aposta*3
+        elif aposta == "desligar":
+            print("O jogo acabou!")
 
-        print("Você tem {0}".format(fichas))
+    fichas == fichas
 
     if jogo == "Any Craps" or jogo == "any craps":
 #Any Craps(disponível em qual fase do jogo): Se a soma der 2, 3 ou 12, o jogador ganha 7 vezes o valor de sua aposta. Caso o contrário, ele perde tudo que apostou.
@@ -76,28 +87,37 @@ if pergunta == "sim" or pergunta == "s" or pergunta == "Sim":
 
         if soma == 2 or soma == 3 or soma == 12:
             fichas += aposta*7
+        elif aposta == "desligar":
+            print("O jogo acabou!")
         else:
             fichas -= aposta
 
-        print("Você tem {0}".format(fichas))
+    fichas == fichas
 
     if jogo == "Twelve" or jogo == "twelve":
 #Twelve(disponível em qual fase do jogo): Se a soma der 12, o jogador ganha 30 o valor de sua aposta. Caso o contrário, ele perde tudo.
         
         aposta = int(input("Quanto você quer apostar?"))
         
-        d1():
+        d1()
         print("A soma deu {0}".format(soma))
 
         if soma == 12:
             fichas += aposta*30
+        elif aposta == "desligar":
+            print("O jogo acabou!")
         else:
-            soma -= aposta
+            fichas -= aposta
         
-        print("Você tem {0}".format(fichas))
-        
+    fichas == fichas
+
+    print("Você tem: {0} fichas".format(fichas))
+
+    if jogo != "Pass Line Bet" or jogo != "pass line bet" or jogo != "Field" or jogo != "field" or jogo != "Any Craps" or jogo != "any craps" or jogo != "Twelve" or jogo != "twelve":
+        print("Digite o nome do jogo corretamente.")
+
 else:
-    print("O jogo acabou.")
+    print("O jogo acabou!")
 
     #colar lá
     # soma_anterior = soma
