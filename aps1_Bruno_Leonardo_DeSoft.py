0f6ab83692d0cd5babfc267932002ba37cbf7aca
brunocp-liseb <63056784+brunocp-liseb@users.noreply.github.com>
2020-04-02 21:02:58 -0300

    Add files via upload

--- aps1_Bruno_Leonardo_DeSoft.py
+++ aps1_Bruno_Leonardo_DeSoft.py
@@ -1,20 +1,7 @@
-import sys
-#sys.exit()
-
-
 import random
 
-
-#sorteio dado 1 e 2 "Come Out"
 soma = 0
 soma2 = 0
-#x = random.randint(1, 6)
-#y = random.randint(1, 6)
-
-#def d1(x,y):
- #   soma = x + y
-  #  return soma
-
 
 gameon = False
 
@@ -23,12 +10,18 @@ fichas = 100
 aposta = 0
 
 print('Regras do Jogo:')
-print('1-E permitido apenas apostas com numeros inteiros.')
-print('2-Nao e permitido sair do jogo durante a fase de apostas (caso nao seja fase de apostas, para sair do jogo basta escrever "desligar").')
+print('1-É permitido apenas apostas com números inteiros.')
+print('2-Não é permitido sair do jogo durante a fase de apostas (caso não seja a fase de apostas, para sair do jogo basta escrever "desligar").')
+print('  ')
+print('Pass Line Bet(disponível apenas na rodada Come Out): Se a soma dos dados for 7 ou 11, você ganha a mesma quantidade de fichas que apostou. Já se a soma dos dados for 2, 3 ou 12(Craps), você perde tudo. Agora, caso a soma dos dados de 4, 5, 6, 8, 9 ou 10, você avança para a próxima fase: fase Point.')
+print('  ')
+print('Field(disponível em qual fase do jogo): Se a soma der 5, 6, 7 ou 8, o jogador perde tudo. Já se a soma der 3, 4, 9, 10, ou 11, o jogador ganha o mesmo valor que apostou.')
+print('  ')
+print('Any Craps(disponível em qual fase do jogo): Se a soma der 2, 3 ou 12, o jogador ganha 7 vezes o valor de sua aposta. Caso o contrário, ele perde tudo que apostou.')
+print('  ')
+print('Twelve(disponível em qual fase do jogo): Se a soma der 12, o jogador ganha 30 o valor de sua aposta. Caso o contrário, ele perde tudo.')
 print('  ')
 print("Você tem: 100 fichas")
-
-
 
 
 pergunta = input("Você quer apostar?")
@@ -47,10 +40,6 @@ while gameon == True and fichas != 0:
         gameon = False
     if jogo != "Pass Line Bet" and jogo != "pass line bet" and jogo != "Field" and jogo != "field" and jogo != "Any Craps" and jogo != "any craps" and jogo != "Twelve" and jogo != "twelve" and jogo != 'desligar' and jogo != 'Desligar':
         print("Digite o nome do jogo corretamente.")
-#Pass Line Bet(disponível apenas na rodada Come Out): Se a soma dos dados for 7 ou 11, você ganha a mesma quantidade de fichas que apostou. Já se a soma dos dados for 2, 3 ou 12(Craps), você perde tudo. Agora, caso a soma dos dados de 4, 5, 6, 8, 9 ou 10, você avança para a proxima fase: fase Point.
-#Field(disponível em qual fase do jogo): Se a soma der 5, 6, 7 ou 8, o jogador perde tudo. Já se a soma der 3, 4, 9, 10, ou 11, o jogador ganha o mesmo valor que apostou.
-#Any Craps(disponível em qual fase do jogo): Se a soma der 2, 3 ou 12, o jogador ganha 7 vezes o valor de sua aposta. Caso o contrário, ele perde tudo que apostou.
-#Twelve(disponível em qual fase do jogo): Se a soma der 12, o jogador ganha 30 o valor de sua aposta. Caso o contrário, ele perde tudo.
     
     if jogo == "Pass Line Bet" or jogo == "pass line bet":
 #Pass Line Bet(disponível apenas na rodada Come Out): Se a soma dos dados for 7 ou 11, você ganha a mesma quantidade de fichas que apostou. Já se a soma dos dados for 2, 3 ou 12(Craps), você perde tudo. Agora, caso a soma dos dados de 4, 5, 6, 8, 9 ou 10, você avança para a proxima fase: fase Point.
@@ -80,13 +69,9 @@ while gameon == True and fichas != 0:
                 if soma2 == point:
                     fichas += aposta
                 elif soma2 == 7:
-                    fichas -= aposta
+                    fichas -= aposta #verificar regras
                 print("A soma deu {0}".format(soma2))
             
-#verificar regras
-            
-        
-
     fichas == fichas
          
     if jogo == "Field" or jogo == "field":
@@ -109,7 +94,6 @@ while gameon == True and fichas != 0:
         elif soma == 12:
             fichas += aposta*3
         
-
     fichas == fichas
 
     if jogo == "Any Craps" or jogo == "any craps":
@@ -129,7 +113,6 @@ while gameon == True and fichas != 0:
         else:
             fichas -= aposta
         
-
     fichas == fichas
 
     if jogo == "Twelve" or jogo == "twelve":
@@ -152,23 +135,8 @@ while gameon == True and fichas != 0:
             fichas -= aposta
         
     fichas == fichas
-    print('Voce tem: {0} fichas.'.format(fichas))
-    
 
-    #if jogo != "Pass Line Bet" or jogo != "pass line bet" or jogo != "Field" or jogo != "field" or jogo != "Any Craps" or jogo != "any craps" or jogo != "Twelve" or jogo != "twelve" or jogo != 'desligar' or jogo != 'Desligar':
-    #    print("Digite o nome do jogo corretamente.")    
+    print('Você tem: {0} fichas.'.format(fichas))  
 
 else:
     print("O jogo acabou!")
-
-    #colar lá
-    # soma_anterior = soma
-    #         while a =False:
-    #             d1()
-    #             if soma_anterior == soma:
-    #                 fichas += aposta
-    #                 a = True
-    #             elif soma == 7:
-    #                 ficha -= aposta
-    #                 a = True
-    #             else:
